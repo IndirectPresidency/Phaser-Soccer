5206c320e19337272ebabb3e3d294d331a324e70
COLDPineapple <Jackson.hymer@gmail.com>
2022-05-04 16:19:20 +0000

    added 2nd player

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -1,8 +1,8 @@
-import player from "../assets/player.png";
+import playerImg from "../assets/player.png";
 import map from "../assets/map.png";
 import goalBack from "../assets/goalBack.png";
 import goalPost from "../assets/goalPost.png";
-import ball from "../assets/ball.png";
+import ballImg from "../assets/ball.png";
 export default class Game extends Phaser.Scene {
 	constructor() {
 		super("game");
@@ -12,23 +12,26 @@ export default class Game extends Phaser.Scene {
 		this.centerX = this.screenW / 2;
 		this.centerY = this.screenH / 2;
 	}
-	
 
 	preload() {
-		this.load.image("player", player);
+		this.load.image("player", playerImg);
 		this.load.image("map", map);
 		this.load.image("goalBack", goalBack);
 		this.load.image("goalPost", goalPost);
-		this.load.image("ball", ball);
+		this.load.image("ball", ballImg);
 	}
 
 	create() {
 		this.map = this.physics.add.sprite(this.centerX, this.centerY, "map");
 		this.cameras.main.setBackgroundColor("#eee");
 
-		this.player = this.physics.add.sprite(100, 100, "player");
+		this.player = this.physics.add.sprite(150, 150, "player");
 		this.player.setCollideWorldBounds(true);
 		this.player.setScale(0.7);
+
+		this.player1 = this.physics.add.sprite(100, 100, "player");
+		this.player1.setCollideWorldBounds(true);
+		this.player1.setScale(0.7);
 
 		this.ball = this.physics.add.sprite(200, 200, "ball");
 		this.ball.setCollideWorldBounds(true);
@@ -58,24 +61,23 @@ export default class Game extends Phaser.Scene {
 
 	update() {
 		const cursors = this.input.keyboard.createCursorKeys();
+		const scene = this;
 
 		this.physics.collide(this.player, this.ball);
+		this.physics.collide(this.player1, this.ball);
 
-		this.physics.collide(this.player, this.goalBack1);
-		this.physics.collide(this.player, this.goalBack2);
+		function goalCollide(object) {
+			scene.physics.collide(object, scene.goalBack1);
+			scene.physics.collide(object, scene.goalBack2);
 
-		this.physics.collide(this.player, this.goalPost1);
-		this.physics.collide(this.player, this.goalPost2);
-		this.physics.collide(this.player, this.goalPost3);
-		this.physics.collide(this.player, this.goalPost4);
-
-		this.physics.collide(this.ball, this.goalBack1);
-		this.physics.collide(this.ball, this.goalBack2);
-
-		this.physics.collide(this.ball, this.goalPost1);
-		this.physics.collide(this.ball, this.goalPost2);
-		this.physics.collide(this.ball, this.goalPost3);
-		this.physics.collide(this.ball, this.goalPost4);
+			scene.physics.collide(object, scene.goalPost1);
+			scene.physics.collide(object, scene.goalPost2);
+			scene.physics.collide(object, scene.goalPost3);
+			scene.physics.collide(object, scene.goalPost4);
+		}
+		goalCollide(this.player);
+		goalCollide(this.player1);
+		goalCollide(this.ball);
 
 		const lowerVelocity = 0.01;
 		const xVel = this.ball.body.velocity.x;
@@ -104,6 +106,27 @@ export default class Game extends Phaser.Scene {
 			this.player.setVelocityY(this.playerSpeed);
 		} else {
 			this.player.setVelocityY(0);
+		}
+
+		let keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+		let keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+		let keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+		let keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+
+		if (keyD.isDown) {
+			this.player1.setVelocityX(this.playerSpeed);
+		} else if (keyA.isDown) {
+			this.player1.setVelocityX(-this.playerSpeed);
+		} else {
+			this.player1.setVelocityX(0);
+		}
+
+		if (keyW.isDown) {
+			this.player1.setVelocityY(-this.playerSpeed);
+		} else if (keyS.isDown) {
+			this.player1.setVelocityY(this.playerSpeed);
+		} else {
+			this.player1.setVelocityY(0);
 		}
 	}
 }
